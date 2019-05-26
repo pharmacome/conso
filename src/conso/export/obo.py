cb8872ca6157d29b8c770ef1c9060766875cdf3f
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-05-26 19:55:44 +0200

    Update OBO export script
    - handles type definitions and relationships correctly
    - uses entity classes as OBO (sub)namespaces, except when defined as ? it is omitted

--- src/conso/export/obo.py
+++ src/conso/export/obo.py
@@ -5,8 +5,9 @@
 import csv
 import datetime
 import os
+from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import List, Mapping, Optional, TextIO, Union
+from typing import Iterable, List, Mapping, Optional, TextIO, Union
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 ROOT = os.path.join(HERE, os.pardir, os.pardir, os.pardir)
@@ -21,9 +22,19 @@ RELATIONS_PATH = os.path.abspath(os.path.join(ROOT, 'relations.tsv'))
 
 OUTPUT_PATH = os.path.join(ROOT, 'export', 'hbp.obo')
 
-INVERSE_RELATIONS = {
-    'is_a': 'inverse_is_a',
-    'part_of': 'has_part',
+BEL_RELATION_ID = 'bel'
+
+typedefs = {
+    'bel': f"""[Typedef]
+id: {BEL_RELATION_ID}
+name: BEL term
+comment: A formulation for a term in Biological Expression language""",
+    'part_of': """[Typedef]
+id: part_of
+name: part of
+namespace: external
+xref: BFO:0000050
+is_transitive: true""",
 }
 
 
@@ -47,7 +58,7 @@ class Synonym:
 
     name: str
     specificity: str
-    references: Optional[List[str]] = None
+    references: List[str] = field(default_factory=list)
 
 
 @dataclass
@@ -55,34 +66,47 @@ class Term:
     """A term in OBO."""
 
     reference: Reference
+    namespace: str
     description: str
     provenance: List[Reference]
-    relationships: Optional[Mapping[str, List[Reference]]] = field(default_factory=dict)
+    parents: List[Reference] = field(default_factory=list)
+    relationships: Mapping[str, List[Reference]] = field(default_factory=lambda: defaultdict(list))
     synonyms: List[Synonym] = field(default_factory=list)
     xrefs: List[Reference] = field(default_factory=list)
 
+    @property
+    def _namespace_normalized(self) -> str:
+        return self.namespace \
+            .replace(' ', '_') \
+            .replace('-', '_') \
+            .replace('(', '') \
+            .replace(')', '')
+
+    def _yield_obo_lines(self) -> Iterable[str]:
+        yield '[Term]'
+        yield f'id: {self.reference.namespace}:{self.reference.identifier}'
+        yield f'name: {self.reference.name}'
+        if self.namespace != '?':
+            yield f'namespace: {self._namespace_normalized}'
+        yield f'''def: "{self.description}" [{', '.join(map(str, self.provenance))}]'''
+
+        for synonym in self.synonyms:
+            synonym_references_string = ', '.join(synonym.references)
+            yield f'synonym: "{synonym.name}" {synonym.specificity} [{synonym_references_string}]'
+
+        for xref in self.xrefs:
+            yield f'xref: {xref}'
+
+        for parent_reference in self.parents:
+            yield f'is_a: {parent_reference}'
+
+        for relationship, relationship_references in self.relationships.items():
+            for relationship_reference in relationship_references:
+                yield f'relationship: {relationship} {relationship_reference}'
+
     def to_obo(self) -> str:
         """Convert this term to an OBO entry."""
-        obo_str = f'''[Term]
-id: {self.reference.namespace}:{self.reference.identifier}
-name: {self.reference.name}
-def: "{self.description}" [{', '.join(map(str, self.provenance))}]
-'''
-        for synonym in self.synonyms or []:
-            synonym_references_string = ', '.join(synonym.references)
-            obo_str += f'synonym: "{synonym.name}" {synonym.specificity} [{synonym_references_string}]\n'
-
-        for xref in self.xrefs or []:
-            if xref.namespace == 'BEL':
-                obo_str += f'bel: {xref.identifier}\n'
-            else:
-                obo_str += f'xref: {xref}\n'
-
-        for relationship, references in (self.relationships or {}).items():
-            for reference in references:
-                obo_str += f'{relationship}: {reference}\n'
-
-        return obo_str
+        return '\n'.join(self._yield_obo_lines()) + '\n'
 
     def __str__(self):  # noqa: D105
         return self.to_obo()
@@ -101,9 +125,10 @@ def get_obo_terms() -> List[Term]:
                     Reference(*pmid.strip().split(':'))
                     for pmid in references.split(',')
                 ],
+                namespace=namespace,
                 description=description,
             )
-            for hbp_identifier, _, name, _, references, description in reader
+            for hbp_identifier, _, name, namespace, references, description in reader
             if name != 'WITHDRAWN'
         }
 
@@ -125,13 +150,17 @@ def get_obo_terms() -> List[Term]:
         reader = csv.reader(file, delimiter='\t')
         _ = next(reader)  # skip the header
         for hbp_id, database, identifier in reader:
-            terms[hbp_id].xrefs.append(Reference(database, identifier))
+            if database.lower() == 'bel':
+                terms[hbp_id].relationships['bel'] = [identifier]
+            else:
+                terms[hbp_id].xrefs.append(Reference(database, identifier))
 
     with open(RELATIONS_PATH) as file:
         reader = enumerate(csv.reader(file, delimiter='\t'), start=1)
         _ = next(reader)  # skip the header
+        handled_relations = {'is_a'} | set(typedefs)
         for line, (source_ns, source_id, source_name, relation, target_ns, target_id, target_name) in reader:
-            if relation not in {'is_a', 'part_of'}:
+            if relation not in handled_relations:
                 print(f'{RELATIONS_PATH} can not handle line {line} because unhandled relation: {relation}')
                 continue
 
@@ -140,13 +169,15 @@ def get_obo_terms() -> List[Term]:
                 continue
 
             if source_ns != HBP:
-                relation = INVERSE_RELATIONS[relation]
-                source_ns, source_id, source_name, target_ns, target_id, target_name = target_ns, target_id, target_name, source_ns, source_id, source_name
+                print(f'{RELATIONS_PATH} can not handle line {line} because of'
+                      f' inverse relation definition to external identifier')
+                continue
 
-            if relation not in terms[source_id].relationships:
-                terms[source_id].relationships[relation] = []
-
-            terms[source_id].relationships[relation].append(Reference(target_ns, target_id, target_name))
+            target = Reference(target_ns, target_id, target_name)
+            if relation == 'is_a':
+                terms[source_id].parents.append(target)
+            else:
+                terms[source_id].relationships[relation].append(target)
 
     return list(terms.values())
 
@@ -158,13 +189,12 @@ def dump_obo_terms(terms: List[Term], file: Union[None, TextIO]) -> None:
 
     print('format-version: 1.2', file=file)
     print(f'date: {date_str}', file=file)
-    print('auto-generated-by: https://github.com/pharmacome/terminology/blob/master/export/obo.py', file=file)
+    print('auto-generated-by: https://github.com/pharmacome/terminology/blob/master/src/conso/export/obo.py', file=file)
+    print('ontology: conso', file=file)
     print('', file=file)
 
-    print('''[Typedef]
-name: bel
-comment: A formulation for a term in Biological Expression language
-''', file=file)
+    for typedef in typedefs.values():
+        print(f'{typedef}\n', file=file)
 
     for term in terms:
         obo_str = term.to_obo()
