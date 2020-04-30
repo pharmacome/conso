f2f04cce99069502ef347091ca56a59257d667be
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-05-01 01:11:15 +0200

    Add chemofog term and make updated outputs

--- src/conso/export/obo.py
+++ src/conso/export/obo.py
@@ -26,13 +26,25 @@ CONSO = 'CONSO'
 
 def get_obo() -> Obo:
     """Get OBO object."""
+    terms, typedefs = get_content()
+    return Obo(
+        format_version='1.2',
+        auto_generated_by='https://github.com/pharmacome/conso/blob/master/src/conso/export/obo.py',
+        ontology='conso',
+        name='Curation of Neurodegeneration Supporting Ontology',
+        iter_terms=lambda: iter(terms),
+        typedefs=typedefs,
+    )
+
+
+def get_content():
+    """Iterate CONSO terms."""
     with open(TYPEDEF_PATH) as file:
         reader = csv.reader(file, delimiter='\t')
         _ = next(reader)  # skip the header
         typedefs = [
             TypeDef(
-                id=identifier,
-                name=name,
+                reference=Reference(prefix=CONSO, identifier=identifier, name=name),
                 namespace=namespace,
                 xrefs=Reference.from_curies(xrefs),
                 is_transitive=transitive == 'true',
@@ -46,7 +58,7 @@ def get_obo() -> Obo:
         _ = next(reader)  # skip the header
         authors: Mapping[str, Reference] = {
             key: Reference(
-                namespace='orcid',
+                prefix='orcid',
                 identifier=orcid_identifier,
                 name=author,
             )
@@ -63,15 +75,16 @@ def get_obo() -> Obo:
                 continue
             terms[conso_id] = Term(
                 reference=Reference(
-                    namespace=CONSO,
+                    prefix=CONSO,
                     identifier=conso_id,
-                    name=name),
+                    name=name,
+                ),
                 provenance=[
                     Reference.from_curie(pmid_curie)
                     for pmid_curie in references.split(',')
                 ],
                 namespace=namespace,
-                description=description,
+                definition=description,
             )
             terms[conso_id].relationships['author'].append(authors[author_key])
 
@@ -87,21 +100,21 @@ def get_obo() -> Obo:
             specificity = (
                 'EXACT' if specificity == '?' else specificity
             )
-            terms[conso_id].synonyms.append(Synonym(synonym, specificity, references))
+            terms[conso_id].synonyms.append(Synonym(synonym, specificity, provenance=references))
 
     with open(XREFS_PATH) as file:
         reader = csv.reader(file, delimiter='\t')
         _ = next(reader)  # skip the header
         for conso_id, database, identifier in reader:
             if database.lower() == 'bel':
-                terms[conso_id].relationships['bel'] = [Reference(namespace='bel', identifier=identifier)]
+                terms[conso_id].relationships['bel'] = [Reference(prefix='bel', identifier=identifier)]
             else:
-                terms[conso_id].xrefs.append(Reference(namespace=database, identifier=identifier))
+                terms[conso_id].xrefs.append(Reference(prefix=database, identifier=identifier))
 
     with open(RELATIONS_PATH) as file:
         reader = enumerate(csv.reader(file, delimiter='\t'), start=1)
         _ = next(reader)  # skip the header
-        handled_relations = {'is_a'} | {typedef.id for typedef in typedefs}
+        handled_relations = {'is_a'} | {typedef.identifier for typedef in typedefs}
         for line, (source_ns, source_id, source_name, relation, target_ns, target_id, target_name) in reader:
             if relation not in handled_relations:
                 print(f'{RELATIONS_PATH} can not handle line {line} because unhandled relation: {relation}')
@@ -116,19 +129,13 @@ def get_obo() -> Obo:
                       f' inverse relation definition to external identifier')
                 continue
 
-            target = Reference(namespace=target_ns, identifier=target_id, name=target_name)
+            target = Reference(prefix=target_ns, identifier=target_id, name=target_name)
             if relation == 'is_a':
                 terms[source_id].parents.append(target)
             else:
                 terms[source_id].relationships[relation].append(target)
 
-    return Obo(
-        format_version='1.2',
-        auto_generated_by='https://github.com/pharmacome/conso/blob/master/src/conso/export/obo.py',
-        ontology='conso',
-        terms=list(terms.values()),
-        typedefs=typedefs,
-    )
+    return terms.values(), typedefs
 
 
 def main(path: Optional[str] = None) -> None:
